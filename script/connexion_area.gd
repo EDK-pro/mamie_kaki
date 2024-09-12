9859d28361a2daafdf9b90c4ad1d497cecea0916
EDK-pro <118174383+EDK-pro@users.noreply.github.com>
2024-09-12 13:22:24 +0200

    Push nouvelles caméra

--- script/connexion_area.gd
+++ script/connexion_area.gd
@@ -3,13 +3,11 @@ extends Area3D
 @export var objet : MeshInstance3D
 @export var light : OmniLight3D
 
-signal arrivee_joueureuse
-signal depart_joueureuse
+@export var selected : bool = false
 
 func _on_body_entered(body: Node3D) -> void:
-	arrivee_joueureuse.emit()
-	objet.visible = true
+	selected = true
 
 
 func _on_body_exited(body: Node3D) -> void:
-	objet.visible = false
+	selected = false
--- script/mamie.gd
+++ script/mamie.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
 
 @export var modele_mamie : MeshInstance3D
-
+@export var guizmo_forward : MeshInstance3D
+@export var guizmo_side : MeshInstance3D
+@export var guizmo : Node3D
 var mouse_sensitivity : float = 0.05
 var souris_derniere_position = Vector3.ZERO
 
@@ -15,8 +17,7 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("Interraction"):
-		pass
+	pass
 	
 func _process(delta: float) -> void:
 	
@@ -30,11 +31,17 @@ func _process(delta: float) -> void:
 
 func bouger_mamie():
 	var input_deplacement = Input.get_vector("avancer","reculer","gauche","droite")
-	var direction_mamie = Vector3(input_deplacement.y,0,input_deplacement.x).rotated(Vector3.UP,camera.rotation.y)
+	var direction_mamie_x = input_deplacement.x * Vector3(guizmo_forward.global_position.x, 0.0, guizmo_forward.global_position.z) 
+	var direction_mamie_z = input_deplacement.y * Vector3(guizmo_side.global_position.x, 0.0, guizmo_side.global_position.z)
+	var direction_mamie = direction_mamie_x + direction_mamie_z
+	#var direction_mamie = Vector3(input_deplacement.y,0,input_deplacement.x).rotated(Vector3.UP,guizmo_forward.rotation.y)
 	if velocity.length() > 0.2:
 		var look_direction: Vector2 = Vector2(velocity.z, velocity.x)
 		modele_mamie.rotation.y = look_direction.angle()
-	velocity = direction_mamie * 2.0
+	velocity = direction_mamie * 4.0
+	
+func rotate_guizmo(rotation_y_voulue : float):
+	guizmo.rotation.y = rotation_y_voulue
 	
 
 func objet_dessus():
